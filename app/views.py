6c57eccd950bcea21e17bfd2ea02b7e3738ce58d
George007361 <gera.illarionov@mail.ru>
2022-04-08 18:08:46 +0300

    Template fixes

--- app/views.py
+++ app/views.py
@@ -31,46 +31,87 @@ ANSWERS = [
     } for i in range(1, 23)
 ]
 
+ACTIVE = [
+    {
+     "name": "George",
+    },
+    {
+        "name": "Andry",
+    },
+    {
+        "name": "John",
+    },
+]
+
+HOTTAGS = [
+    {
+        "name": "Summer",
+        "level": 1,
+    },
+    {
+        "name": "Winter",
+        "level": 3,
+    },
+    {
+        "name": "Computer",
+        "level": 3,
+    },
+
+    {
+        "name": "Cars",
+        "level": 2,
+    },
+
+    {
+        "name": "Session",
+        "level": 1,
+    },
+
+    {
+        "name": "VK",
+        "level": 2,
+    },
+]
 
 def index(request):
     paginator = Paginator(QUESTIONS, 20)
     page = request.GET.get('page')
     content = paginator.get_page(page)
-    return render(request, "index.html", {'questions_list': content})
+    return render(request, "index.html", {'active_users': ACTIVE, 'hot_tags': HOTTAGS, 'questions_list': content})
 
 
 def ask(request):
-    return render(request, "ask.html")
+    return render(request, "ask.html", {"active_users": ACTIVE, 'hot_tags': HOTTAGS})
 
 
 def settings(request):
-    return render(request, "settings.html")
+    return render(request, "settings.html", {"active_users": ACTIVE, 'hot_tags': HOTTAGS})
 
 
 def question(request, i: int):
     paginator = Paginator(ANSWERS, 5)
     page = request.GET.get('page')
     content = paginator.get_page(page)
-    return render(request, "question_page.html", {"question": QUESTIONS[i - 1], "answers_list":content})
+    return render(request, "question_page.html", {"question": QUESTIONS[i - 1], "answers_list":content, "active_users": ACTIVE, 'hot_tags': HOTTAGS})
 
 
 def hot(request):
     paginator = Paginator(QUESTIONS, 5)
     page = request.GET.get('page')
     content = paginator.get_page(page)
-    return render(request, "hot.html", {"questions_list": content})
+    return render(request, "hot.html", {"questions_list": content, "active_users": ACTIVE, 'hot_tags': HOTTAGS})
 
 
 def tag(request, i:str):
     paginator = Paginator(QUESTIONS, 8)
     page = request.GET.get('page')
     content = paginator.get_page(page)
-    return render(request, "tag.html", {"tag": i, "questions_list": content})
+    return render(request, "tag.html", {"tag": i, "questions_list": content, "active_users": ACTIVE, 'hot_tags': HOTTAGS})
 
 
 def login(request):
-    return render(request, "login.html")
+    return render(request, "login.html", {"active_users": ACTIVE, 'hot_tags': HOTTAGS})
 
 
 def signup(request):
-    return render(request, "register.html")
+    return render(request, "register.html", {"active_users": ACTIVE, 'hot_tags': HOTTAGS})
